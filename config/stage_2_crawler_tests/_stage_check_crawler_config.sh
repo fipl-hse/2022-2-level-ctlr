19c5d2144e577a33d3a72d4ebd0eef019d84ba47
Alexander Demidovsiy <monadv@yandex.ru>
2023-03-09 12:31:56 +0300

    skip tests for admins

--- config/stage_2_crawler_tests/_stage_check_crawler_config.sh
+++ config/stage_2_crawler_tests/_stage_check_crawler_config.sh
@@ -8,6 +8,12 @@ echo $TARGET_SCORE
 
 source venv/bin/activate
 
+IS_ADMIN=$(python config/is_admin.py --pr_name "$1")
+if [ "$REPOSITORY_TYPE" == "public" ] && [ "$IS_ADMIN" == 'YES' ] ; then
+  echo '[skip-lab] option was enabled, skipping check...'
+  exit 0
+fi
+
 if [[ ${TARGET_SCORE} != 0 ]]; then
   python -m pytest -m "mark10 and stage_2_1_crawler_config_check" --capture=no
 else
--- config/stage_2_crawler_tests/_stage_crawler_checks.sh
+++ config/stage_2_crawler_tests/_stage_crawler_checks.sh
@@ -5,6 +5,12 @@ echo "Crawler checks"
 
 TARGET_SCORE=$(bash config/get_mark.sh lab_5_scrapper)
 
+IS_ADMIN=$(python config/is_admin.py --pr_name "$1")
+if [ "$REPOSITORY_TYPE" == "public" ] && [ "$IS_ADMIN" == 'YES' ] ; then
+  echo '[skip-lab] option was enabled, skipping check...'
+  exit 0
+fi
+
 if [[ ${TARGET_SCORE} != 0 ]]; then
   bash config/stage_2_crawler_tests/s2_2_check_crawler.sh
 else
