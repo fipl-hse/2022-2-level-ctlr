72831c764ba30631db21051e3718821cc29ec3e4
Alexander Demidovsiy <monadv@yandex.ru>
2023-03-11 15:39:34 +0300

    first appearance

---
+++ lab_5_scrapper/scrapper.py
@@ -0,0 +1,12 @@
+# pylint: disable=too-many-arguments, too-many-instance-attributes
+"""
+Crawler implementation
+"""
+
+
+def main() -> None:
+    print('Hello, world!')
+
+
+if __name__ == '__main__':
+    main()
