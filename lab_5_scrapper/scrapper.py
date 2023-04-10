1209bb6447edd687d324e316bdf2d82c5668fad1
YuliaVodopyanova <vodopyanovaye@mail.ru>
2023-04-10 18:47:23 +0300

    changes

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -7,8 +7,8 @@ import re
 import shutil
 import time
 from pathlib import Path
-from typing import Pattern, Union
 from random import randint
+from typing import Pattern, Union
 
 import requests
 from bs4 import BeautifulSoup
@@ -16,8 +16,8 @@ from bs4 import BeautifulSoup
 from core_utils.article.article import Article
 from core_utils.article.io import to_meta, to_raw
 from core_utils.config_dto import ConfigDTO
-from core_utils.constants import ASSETS_PATH, CRAWLER_CONFIG_PATH, NUM_ARTICLES_UPPER_LIMIT, TIMEOUT_LOWER_LIMIT, \
-    TIMEOUT_UPPER_LIMIT
+from core_utils.constants import (ASSETS_PATH, CRAWLER_CONFIG_PATH, NUM_ARTICLES_UPPER_LIMIT, TIMEOUT_LOWER_LIMIT,
+                                  TIMEOUT_UPPER_LIMIT)
 
 
 class IncorrectSeedURLError(Exception):
