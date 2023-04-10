1faa7c8209aa2194bd89693c53ddead0622beaa0
Sergo-M <mr.sermoz@gmail.com>
2023-04-10 23:52:41 +0300

    time sleep added

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -3,10 +3,12 @@ Crawler implementation
 """
 import datetime
 import json
+import random
 import re
 import shutil
+import time
 from pathlib import Path
-from typing import Union
+from typing import Pattern, Union
 
 import requests
 from bs4 import BeautifulSoup
@@ -181,6 +183,7 @@ def make_request(url: str, config: Config) -> requests.models.Response:
     Delivers a response from a request
     with given configuration
     """
+    time.sleep(random.uniform(1.1, 4.4))
     response = requests.get(url, headers=config.get_headers(), timeout=config.get_timeout(),
                             verify=config.get_verify_certificate())
     response.encoding = response.apparent_encoding
