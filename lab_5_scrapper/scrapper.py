177a1b045d5a3dc47a0cd3b7fca28ba0fed05050
AndreyKarnauhov <91670823+AndreyKarnauhov@users.noreply.github.com>
2023-04-10 08:00:20 +0300

    reordering inputs

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -1,16 +1,16 @@
 """
 Crawler implementation
 """
-from typing import Pattern, Union
-from pathlib import Path
-import requests
+from core_utils.article.article import Article
 from bs4 import BeautifulSoup
 import datetime
-from core_utils.article.article import Article
 from core_utils.config_dto import ConfigDTO
 from core_utils.constants import CRAWLER_CONFIG_PATH
 import json
+from pathlib import Path
+from typing import Pattern, Union
 import re
+import requests
 
 
 class IncorrectSeedURLError(Exception):
