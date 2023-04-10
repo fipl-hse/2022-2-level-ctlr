8be43fc86ebb55c551384218e00922ce4e869dd8
AndreyKarnauhov <91670823+AndreyKarnauhov@users.noreply.github.com>
2023-04-10 07:55:15 +0300

    stage 1

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -2,7 +2,44 @@
 Crawler implementation
 """
 from typing import Pattern, Union
-# tis where my lab gonna be
+from pathlib import Path
+import requests
+from bs4 import BeautifulSoup
+import datetime
+from core_utils.article.article import Article
+from core_utils.config_dto import ConfigDTO
+from core_utils.constants import CRAWLER_CONFIG_PATH
+import json
+import re
+
+
+class IncorrectSeedURLError(Exception):
+    pass
+
+
+class NumberOfArticlesOutOfRangeError(Exception):
+    pass
+
+
+class IncorrectNumberOfArticlesError(Exception):
+    pass
+
+
+class IncorrectHeadersError(Exception):
+    pass
+
+
+class IncorrectEncodingError(Exception):
+    pass
+
+
+class IncorrectTimeoutError(Exception):
+    pass
+
+
+class IncorrectVerifyError(Exception):
+    pass
+
 
 class Config:
     """
@@ -13,62 +50,97 @@ class Config:
         """
         Initializes an instance of the Config class
         """
-        pass
+        self.path_to_config = path_to_config
+        self.config = self._extract_config_content()
+
+        self._seed_urls = self.config.seed_urls
+        self._num_articles = self.config.total_articles
+        self._headers = self.config.headers
+        self._encoding = self.config.encoding
+        self._timeout = self.config.timeout
+        self._should_verify_certificate = self.config.should_verify_certificate
+        self._headless_mode = self.config.headless_mode
+
+        self._validate_config_content()
 
     def _extract_config_content(self) -> ConfigDTO:
         """
         Returns config values
         """
-        pass
+        with open(self.path_to_config, 'r') as config:
+            content = json.load(config)
+            extracted = ConfigDTO(
+                content['seed_urls'],
+                content['total_articles_to_find_and_parse'],
+                content['headers'],
+                content['encoding'],
+                content['timeout'],
+                content['should_verify_certificate'],
+                content['headless_mode'])
+            return extracted
 
     def _validate_config_content(self) -> None:
         """
         Ensure configuration parameters
         are not corrupt
         """
-        pass
+        if not isinstance(self._seed_urls, list) or not all(isinstance(url, str) for url in self._seed_urls) or \
+                not all(re.search('https?://w?w?w?.', url) for url in self._seed_urls):
+            raise IncorrectSeedURLError
+        if not isinstance(self._num_articles, int) or isinstance(self._num_articles, bool) or self._num_articles < 1:
+            raise IncorrectNumberOfArticlesError
+        if self._num_articles > 150:
+            raise NumberOfArticlesOutOfRangeError
+        if not isinstance(self._headers, dict):
+            raise IncorrectHeadersError
+        if not isinstance(self._encoding, str):
+            raise IncorrectEncodingError
+        if not isinstance(self._timeout, int) or self._timeout <= 0 or self._timeout >= 60:
+            raise IncorrectTimeoutError
+        if not isinstance(self._should_verify_certificate, bool) or not isinstance(self._headless_mode, bool):
+            raise IncorrectVerifyError
 
     def get_seed_urls(self) -> list[str]:
         """
         Retrieve seed urls
         """
-        pass
+        return self.config.seed_urls
 
     def get_num_articles(self) -> int:
         """
         Retrieve total number of articles to scrape
         """
-        pass
+        return self.config.total_articles
 
     def get_headers(self) -> dict[str, str]:
         """
         Retrieve headers to use during requesting
         """
-        pass
+        return self.config.headers
 
     def get_encoding(self) -> str:
         """
         Retrieve encoding to use during parsing
         """
-        pass
+        return self.config.encoding
 
     def get_timeout(self) -> int:
         """
         Retrieve number of seconds to wait for response
         """
-        pass
+        return self.config.timeout
 
     def get_verify_certificate(self) -> bool:
         """
         Retrieve whether to verify certificate
         """
-        pass
+        return self.config.should_verify_certificate
 
     def get_headless_mode(self) -> bool:
         """
         Retrieve whether to use headless mode
         """
-        pass
+        return self.config.headless_mode
 
 
 def make_request(url: str, config: Config) -> requests.models.Response:
@@ -158,7 +230,7 @@ def main() -> None:
     """
     Entrypoint for scrapper module
     """
-    pass
+    configuration = Config(path_to_config=CRAWLER_CONFIG_PATH)
 
 
 if __name__ == "__main__":
