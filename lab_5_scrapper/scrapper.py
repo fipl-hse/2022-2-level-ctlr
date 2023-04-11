62205c399f255f4787070278c73877ad9b4bb85f
Darya Balba <dpbalba@edu.hse.ru>
2023-04-11 22:20:10 +0300

    for 6

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -6,12 +6,11 @@ import requests
 import datetime
 import re
 import shutil
-import time
 from bs4 import BeautifulSoup
 from pathlib import Path
 from typing import Pattern, Union
 from core_utils.config_dto import ConfigDTO
-from core_utils.constants import CRAWLER_CONFIG_PATH
+from core_utils.constants import CRAWLER_CONFIG_PATH, ASSETS_PATH, TIMEOUT_UPPER_LIMIT, TIMEOUT_LOWER_LIMIT
 from core_utils.article.article import Article
 from core_utils.article.io import to_raw, to_meta
 
@@ -77,6 +76,8 @@ class Config:
         with open(self.path_to_config, 'r', encoding='utf-8') as json_file:
             self.content = json.load(json_file)
 
+        self._validate_config_content()
+
         self._seed_urls = self.content['seed_urls']
         self._num_articles = self.content['total_articles_to_find_and_parse']
         self._headers = self.content['headers']
@@ -85,105 +86,98 @@ class Config:
         self._should_verify_certificate = self.content['should_verify_certificate']
         self._headless_mode = self.content['headless_mode']
 
-        self._validate_config_content()
         self.config_obj = self._extract_config_content()
 
     def _extract_config_content(self) -> ConfigDTO:
         """
         Returns config values
         """
+        return ConfigDTO(**self.content)
 
-        return ConfigDTO(self._seed_urls,
-                         self._num_articles,
-                         self._headers,
-                         self._encoding,
-                         self._timeout,
-                         self._should_verify_certificate,
-                         self._headless_mode)
 
     def _validate_config_content(self) -> None:
         """
         Ensure configuration parameters
         are not corrupt
         """
-        if not isinstance(self._seed_urls, list):
+        if not isinstance(self.content['seed_urls'], list):
             raise IncorrectSeedURLError
 
-        for element in self._seed_urls:
+        for element in self.content['seed_urls']:
             if not isinstance(element, str):
                 raise IncorrectSeedURLError
 
             if 'https://' not in element:
-            #if not re.match(r'https://', element):
                 raise IncorrectSeedURLError
 
-        if not isinstance(self._num_articles, int)\
-                or isinstance(self._num_articles, bool) or self._num_articles <= 0:
+        if not isinstance(self.content['total_articles_to_find_and_parse'], int)\
+                or isinstance(self.content['total_articles_to_find_and_parse'], bool)\
+                or self.content['total_articles_to_find_and_parse'] <= 0:
             raise IncorrectNumberOfArticlesError("Total number of articles to parse is not integer")
 
-        if self._num_articles < 1 \
-                or self._num_articles > 150:
+        if self.content['total_articles_to_find_and_parse'] < 1 \
+                or self.content['total_articles_to_find_and_parse'] > 150:
             raise NumberOfArticlesOutOfRangeError("Total number of articles is out of range from 1 to 150")
 
-        if not isinstance(self._headers, dict):
+        if not isinstance(self.content['headers'], dict):
             raise IncorrectHeadersError("Headers are not in a form of dictionary")
 
-        if not isinstance(self._encoding, str):
+        if not isinstance(self.content['encoding'], str):
             raise IncorrectEncodingError("Encoding must be specified as a string")
 
-        if not isinstance(self._timeout, int):
+        if not isinstance(self.content['timeout'], int):
             raise IncorrectTimeoutError
 
-        if self._timeout <= 0 or self._timeout > 60:
+        if self.content['timeout'] <= TIMEOUT_LOWER_LIMIT or self.content['timeout'] > TIMEOUT_UPPER_LIMIT:
             raise IncorrectTimeoutError("Timeout value must be a positive integer less than 60")
 
-        if not isinstance(self._headless_mode, bool):
+        if not isinstance(self.content['headless_mode'], bool):
             raise IncorrectVerifyError
 
-        if not isinstance(self._should_verify_certificate, bool):
+        if not isinstance(self.content['should_verify_certificate'], bool):
             raise IncorrectVerifyError("Verify certificate value must either be True or False")
 
     def get_seed_urls(self) -> list[str]:
         """
         Retrieve seed urls
         """
-        return self.config_obj.seed_urls
+        return self._seed_urls
 
     def get_num_articles(self) -> int:
         """
         Retrieve total number of articles to scrape
         """
-        return self.config_obj.total_articles
+        return self._num_articles
 
     def get_headers(self) -> dict[str, str]:
         """
         Retrieve headers to use during requesting
         """
-        return self.config_obj.headers
+        return self._headers
 
     def get_encoding(self) -> str:
         """
         Retrieve encoding to use during parsing
         """
-        return self.config_obj.encoding
+        return self._encoding
 
     def get_timeout(self) -> int:
         """
         Retrieve number of seconds to wait for response
         """
-        return self.config_obj.timeout
+        return self._timeout
 
     def get_verify_certificate(self) -> bool:
         """
         Retrieve whether to verify certificate
         """
-        return self.config_obj.should_verify_certificate
+        return self._should_verify_certificate
 
     def get_headless_mode(self) -> bool:
         """
         Retrieve whether to use headless mode
         """
-        return self.config_obj.headless_mode
+        return self._headless_mode
 
 
 def make_request(url: str, config: Config) -> requests.models.Response:
@@ -192,9 +186,9 @@ def make_request(url: str, config: Config) -> requests.models.Response:
     with given configuration
     """
 
-    response = requests.get(url, config.get_headers())
-    time.sleep(config.get_timeout())
-
+    response = requests.get(url, headers=config.get_headers(), timeout=config.get_timeout(),
+                            verify=config.get_verify_certificate())
+    response.encoding = config.get_encoding()
     return response
 
 
@@ -217,24 +211,32 @@ class Crawler:
         """
         Finds and retrieves URL from HTML
         """
-        return article_bs.get('href')
+        link = article_bs.get('href')
+        if link:
+            return link
+        return ''
 
     def find_articles(self) -> None:
         """
         Finds articles
         """
         for element in self.seed_urls:
-            response = make_request(element, self.config)  # making request for each link
-            if response.status_code != 200:  # i'm not sure whether i need this check
+            response = make_request(element, self.config)
+            if response.status_code != 200:
                 continue
 
             main_bs = BeautifulSoup(response.text, 'lxml')
-            paragraphs = main_bs.find_all('h1', {'class': 'elementor-heading-title elementor-size-default'}) # searching paragraphs
+            paragraphs = main_bs.find_all('h1', {'class': 'elementor-heading-title elementor-size-default'})
 
             for each_par in paragraphs:
-                ans = each_par.find_all('a')  # searching for all 'a' tags
+                if len(self.urls) > self.config.get_num_articles():
+                    return
+                ans = each_par.find_all('a')
                 for elem in ans:
-                    self.urls.append(self._extract_url(article_bs=elem))  # getting the link
+                    link = self._extract_url(article_bs=elem)
+                    if not link or link in self.urls:
+                        continue
+                    self.urls.append(link)
 
     def get_search_urls(self) -> list:
         """
@@ -264,46 +266,43 @@ class HTMLParser:
         """
         texts_bs = article_soup.find('div', {
             'class':
-                'elementor-element elementor-element-6732342 elementor-widget elementor-widget-theme-post-content'})
+                'elementor-section-wrap'})
         texts = texts_bs.find_all('p')
-        result = ''
+        result = []
         for el in texts:
-            txt = el.text
-            txt = txt.replace('\n\t\t\t\t', '')
-            txt = txt.replace('\t\t\t', '')
-            if txt != '\xa0':
-                result = ' '.join([result, txt])
-        self.article.text = result
+            result.append(el.get_text(strip=True))
+        self.article.text = ' '.join(result)
 
     def _fill_article_with_meta_information(self, article_soup: BeautifulSoup) -> None:
         """
         Finds meta information of article
         """
         title_bs = article_soup.find('h1', {'class': 'elementor-heading-title elementor-size-default'})
-        self.article.title = title_bs.text
+        if title_bs:
+            self.article.title = title_bs.text
 
         date_bs = article_soup.find('span',
-                                    {'class': 'elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-date'})
-        date_txt = re.search(r'\d{2}/\d{2}/\d{4}', date_bs.text)
-        self.article.date = date_txt[0]
+                                    {'class':
+                                         'elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-date'})
+        if date_bs:
+            date_txt = re.search(r'\d{2}/\d{2}/\d{4}', date_bs.text)
+
+        self.article.date = self.unify_date_format(date_txt[0])
 
         auth_bs = article_soup.find('span', {
             'class': 'elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-author'})
         auth_txt = re.search(r'\w+\s\w+', auth_bs.text)
-        self.article.author = auth_txt[0]
+        self.article.author = [auth_txt[0]]
 
         topic_bs = article_soup.find('h3', {'class': 'elementor-heading-title elementor-size-default'})
         topic_txt = topic_bs.find('span').text
-        self.article.topics = topic_txt
-
-        self.article.article_id = self.article_id
-        self.article.url = self.full_url
+        self.article.topics = [topic_txt]
 
     def unify_date_format(self, date_str: str) -> datetime.datetime:
         """
         Unifies date format
         """
-        return datetime.datetime.strptime(date_str, '%d/%m/%y')
+        return datetime.datetime.strptime(date_str, '%d/%m/%Y')
 
     def parse(self) -> Union[Article, bool, list]:
         """
@@ -333,16 +332,16 @@ def main() -> None:
     """
     Entrypoint for scrapper module
     """
-    base_path = Path('C:\\pyproj\\hse\\2022-2-level-ctlr\\tmp\\articles')
-    prepare_environment(base_path)
+    prepare_environment(ASSETS_PATH)
     configuration = Config(path_to_config=CRAWLER_CONFIG_PATH)
     crawler = Crawler(config=configuration)
     crawler.find_articles()
-    for i, full_url in enumerate(configuration.get_seed_urls(), 1):
+    for i, full_url in enumerate(crawler.urls, start=1):
         parser = HTMLParser(full_url=full_url, article_id=i, config=configuration)
         article = parser.parse()
-        to_raw(article)
-        to_meta(article)
+        if isinstance(article, Article):
+            to_raw(article)
+            to_meta(article)
 
 if __name__ == "__main__":
     main()
