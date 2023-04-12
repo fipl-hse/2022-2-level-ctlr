f1cc35e339abc40525208e4a1e52c1e04571bcd6
pdshh <pdshamanina@gmail.com>
2023-04-12 03:43:51 +0300

    for 6 check

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -7,6 +7,7 @@ import re
 import shutil
 from pathlib import Path
 from typing import Pattern, Union
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,7 +17,6 @@ from core_utils.config_dto import ConfigDTO
 from core_utils.constants import (ASSETS_PATH, CRAWLER_CONFIG_PATH,
                                   NUM_ARTICLES_UPPER_LIMIT,
                                   TIMEOUT_LOWER_LIMIT, TIMEOUT_UPPER_LIMIT)
-
 
 
 class IncorrectSeedURLError(Exception):
