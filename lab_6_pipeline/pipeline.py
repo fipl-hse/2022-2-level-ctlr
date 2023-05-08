cd974ea598e3287de1102b797d0a6a43e10d2f70
Chris Nigamedzyanova <kristinanigamedzanova@gmail.com>
2023-05-08 21:31:33 +0300

    fix

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -5,10 +5,11 @@ from pathlib import Path
 from pymystem3 import Mystem
 import re
 from typing import List
+import string
 
 from core_utils.article.article import SentenceProtocol
 from core_utils.article.ud import OpencorporaTagProtocol, TagConverter
-from core_utils.article.io import from_raw, from_meta, to_cleaned, to_conllu
+from core_utils.article.io import from_raw, to_cleaned, to_conllu
 from core_utils.constants import ASSETS_PATH
 from core_utils.article.article import Article, split_by_sentence
 
@@ -39,6 +40,7 @@ class CorpusManager:
         self._storage = {}
         self.path_to_raw_txt_data = path_to_raw_txt_data
         self._validate_dataset()
+        self._scan_dataset()
 
     def _validate_dataset(self) -> None:
         """
@@ -51,32 +53,30 @@ class CorpusManager:
         if not self.path_to_raw_txt_data.is_dir():
             raise NotADirectoryError
 
-        if not self.path_to_raw_txt_data.iterdir():
+        if not list(self.path_to_raw_txt_data.iterdir()):
             raise EmptyDirectoryError
 
-
-        texts = list(self.path_to_raw_txt_data.glob('*.txt'))
-        meta_info = list(self.path_to_raw_txt_data.glob('*.json'))
+        texts = list(self.path_to_raw_txt_data.glob('*_raw.txt'))
+        meta_info = list(self.path_to_raw_txt_data.glob('*_meta.json'))
         right_texts = sorted([int(re.match(r'\d+', i.name).group()) for i in texts])
 
         if len(right_texts) != len(meta_info):
             raise InconsistentDatasetError
 
-        for i in texts:
+        for i in texts + meta_info:
             if not i.stat().st_size:
                 raise InconsistentDatasetError
             if sorted([int(re.match(r'\d+', i.name).group()) for i in texts]) \
-                    != list(i for i in range(1, len(texts)+1)):
+                    != list(range(1, len(texts)+1)):
                 raise InconsistentDatasetError
 
     def _scan_dataset(self) -> None:
         """
         Register each dataset entry
         """
-        for file in (list(self.path_to_raw_txt_data.glob('*.txt'))):
+        for file in (list(self.path_to_raw_txt_data.glob('**/*.txt'))):
             num = int(re.match(r'\d+', file.name).group())
-            if not num:
-                continue
+            print(num)
             self._storage[num] = from_raw(file)
 
 
@@ -111,6 +111,7 @@ class ConlluToken:
         """
         self._text = text
         self.position = 0
+        self._morphological_parameters = MorphologicalTokenDTO()
 
     def set_morphological_parameters(self, parameters: MorphologicalTokenDTO) -> None:
         """
@@ -134,18 +135,17 @@ class ConlluToken:
         pos = self._morphological_parameters.pos
         xpos = '_'
         feats = '_'
-        head = 0
+        head = '0'
         deprel = '_'
         deps = '_'
         misc = '_'
-        parameters = [position, text, lemma, pos, xpos, feats, head, deprel, deps, misc]
-        return '\t'.join(parameters)
+        return '\t'.join([str(position), text, lemma, pos, xpos, feats, head, deprel, deps, misc])
 
     def get_cleaned(self) -> str:
         """
         Returns lowercase original form of a token
         """
-        return re.sub(r'[^\s\w]+', ' ', self._text).lower()
+        return re.sub(r'[^\s\w]+', '', self._text).lower()
 
 
 class ConlluSentence(SentenceProtocol):
@@ -198,8 +198,6 @@ class MystemTagConverter(TagConverter):
         """
 
 
-
-
     def convert_pos(self, tags: str) -> str:  # type: ignore
         """
         Extracts and converts the POS from the Mystem tags into the UD format
@@ -239,18 +237,21 @@ class MorphologicalAnalysisPipeline:
         """
         sentences = split_by_sentence(text)
         conllu_sent = []
-        result = [i for i in self._mystem.analyze(text) if 'analysis' in i]
-        for sent_id, sentence in enumerate(sentences, start=1):
+        punct = '!"#$%&()*+,-/:;<=>?@[\]^_`{|}~'
+        for sent_id, sentence in enumerate(sentences):
             conllu_tokens = []
-            for token_id, token in enumerate(sentence):
-                conllu_token = ConlluToken(token)
-                conllu_tokens.append(conllu_token)
-                lemma = result[token_id]['analysis'][0]['lex']
-                pos = re.match(r'[A-Z]+', result[token_id]['analysis'][0]['gr']).group()
-                tags = result[token_id]['analysis'][0]['gr']
-                parameters = MorphologicalTokenDTO(lemma, pos, tags)
-                conllu_token.set_morphological_parameters(parameters)
-                conllu_tokens.append(conllu_token)
+            result = [i for i in self._mystem.analyze(sentence) if (i['text'] not in punct and i['text'].strip())]
+            for token_id, token in enumerate(result):
+                conllu_token = ConlluToken(token['text'])
+                if token.get('analysis'):
+                    conllu_tokens.append(conllu_token)
+                    print(token)
+                    lemma = token['analysis'][0]['lex']
+                    pos = re.match(r'[A-Z]+', token['analysis'][0]['gr']).group()
+                    tags = token['analysis'][0]['gr']
+                    parameters = MorphologicalTokenDTO(lemma, pos, tags)
+                    conllu_token.set_morphological_parameters(parameters)
+                    conllu_tokens.append(conllu_token)
             conllu_sent.append(ConlluSentence(sent_id, sentence, conllu_tokens))
         return conllu_sent
 
@@ -261,11 +262,10 @@ class MorphologicalAnalysisPipeline:
         """
         articles = self._corpus.get_articles().values()
         for article in articles:
-            filled_article = from_raw(self._corpus.path_to_raw_txt_data, article)
             sentences = self._process(article.get_raw_text())
-            filled_article.set_conllu_sentences(sentences)
-            to_cleaned(filled_article)
-            to_conllu(filled_article)
+            article.set_conllu_sentences(sentences)
+            to_cleaned(article)
+            to_conllu(article)
 
 
 
