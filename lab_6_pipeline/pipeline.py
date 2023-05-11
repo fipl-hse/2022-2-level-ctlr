063d140a0cf9a7675f179628433521962bdeadb8
PolinaDomozhirova <pvdomozhirova@edu.hse.ru>
2023-05-11 20:49:44 +0300

    changes

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -291,13 +291,10 @@ class MorphologicalAnalysisPipeline:
 
             for token in mystem_analysis:
                 if not word_regex.match(token['text']):
-                    if token['text'] == '.':
-                        conllu_token = ConlluToken('.')
-                        conllu_token.set_morphological_parameters(MorphologicalTokenDTO('.', 'PUNCT'))
-                        conllu_token.set_position(len(conllu_tokens) + 1)
-                        conllu_tokens.append(conllu_token)
                     continue
                 sentence_counter += 1
+
+                token['text'] = token['text'].replace(' ', '')
 
                 if token['text'].isalpha():
                     if 'analysis' in token and token['analysis']:
