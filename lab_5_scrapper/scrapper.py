f22d6c21e069539a8cef5da1a27a6a70117f89d4
yourMasya <mmatvs@mail.ru>
2023-05-12 09:42:03 +0300

    Update scrapper.py

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -274,7 +274,7 @@ class HTMLParser:
                                                                 class_="single-header__rubric")]
         if topics:
             self.article.topics = topics[:-1]
-            self.article.author = [-1]
+            self.article.author = topics[-1]
 
     def unify_date_format(self, date_str: str) -> datetime.datetime:
         """
