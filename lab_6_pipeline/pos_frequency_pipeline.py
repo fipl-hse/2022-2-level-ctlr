7c612707d73b816d83458cf4f85c2fb45cf076bf
daryabalba <dpbalba@edu.hse.ru>
2023-05-18 15:35:00 +0300

    for 10

--- lab_6_pipeline/pos_frequency_pipeline.py
+++ lab_6_pipeline/pos_frequency_pipeline.py
@@ -4,8 +4,10 @@ Implementation of POSFrequencyPipeline for score ten only.
 from typing import Optional
 from pathlib import Path
 
-from core_utils.article.article import Article
+from core_utils.article.article import Article, ArtifactType
 from lab_6_pipeline.pipeline import ConlluToken, CorpusManager
+from core_utils.article.ud import extract_sentences_from_raw_conllu
+from core_utils.constants import ASSETS_PATH
 
 class EmptyFileError(Exception):
     """
@@ -16,6 +18,17 @@ def from_conllu(path: Path, article: Optional[Article] = None) -> Article:
     """
     Populates the Article abstraction with all information from the conllu file
     """
+    with open(path, 'r', encoding='utf8') as conllu_file:
+        conllu_text = conllu_file.read()
+
+    conllu_sentences = extract_sentences_from_raw_conllu(conllu_text)
+
+    if not article:
+        article = Article()
+    article.set_conllu_sentences(conllu_sentences)
+
+    return article
+
 
 
 def _parse_conllu_token(token_line: str) -> ConlluToken:
@@ -38,11 +51,21 @@ class POSFrequencyPipeline:
         """
         Initializes PosFrequencyPipeline
         """
+        self._corpus_manager = corpus_manager
 
     def run(self) -> None:
         """
         Visualizes the frequencies of each part of speech
         """
+        articles = self._corpus_manager.get_articles()
+        for article in articles.values():
+            # print(article)
+            conllu_path = article.get_file_path(kind=ArtifactType.POS_CONLLU)
+            conllu_info = from_conllu(conllu_path, article)
+            print(conllu_info.get_conllu_text(True))
+
+
+
 
     def _count_frequencies(self, article: Article) -> dict[str, int]:
         """
@@ -54,6 +77,9 @@ def main() -> None:
     """
     Entrypoint for the module
     """
+    corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
+    posfrequency_pipeline = POSFrequencyPipeline(corpus_manager)
+    posfrequency_pipeline.run()
 
 
 if __name__ == "__main__":
