ee64d75c354a42526410c03a65baf1f9441575a1
Alyona Gosteva <gost-alyona@yandex.ru>
2023-05-19 00:28:56 +0500

    mark 6

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -257,6 +257,7 @@ class MorphologicalAnalysisPipeline:
             for token in mystem_sentence:
                 if not re.match(r'\w+|[.]', token['text']):
                     continue
+                text = re.sub(r'\s', '', token['text'])
                 token_counter += 1
                 if token['text'].isalpha() and token.get("analysis"):
                     lemma = token['analysis'][0]['lex']
@@ -266,13 +267,13 @@ class MorphologicalAnalysisPipeline:
                     lemma = token['text']
                     pos = 'NUM'
                 elif '.' in token['text']:
-                    lemma = token['text'].strip()
+                    lemma = text
                     pos = 'PUNCT'
                 else:
                     lemma = token['text']
                     pos = 'X'
 
-                conllu_token = ConlluToken(token['text'])
+                conllu_token = ConlluToken(text)
                 conllu_token.set_position(token_counter)
                 conllu_token.set_morphological_parameters(MorphologicalTokenDTO(lemma, pos, ''))
                 conllu_tokens.append(conllu_token)
