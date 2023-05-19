001b390cbe8a10b051660956831978c5253be526
Maydaymywaytoheaven <tobakunina@edu.hse.ru>
2023-05-19 09:29:00 +0500

    code style bullying

--- seminars/семинарааа.py
+++ seminars/семинарааа.py
@@ -1,19 +1,19 @@
-import pymystem3
-
-copypasta = ''
-punct = '''!"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~'''
-text = ''
-for char in copypasta.lower():
-    if char not in punct:
-        text += char
-
-print(text)
-
-analysed = pymystem3.Mystem().analyze(text)
-counter = 0
-
-for i in analysed:
-    if 'analysis' in i and i['analysis'] and 'gr' in i['analysis'][0] and i['analysis'][0]['gr'][:2] == 'S,':
-        counter += 1
-        print(i['analysis'][0])
-print(counter)
+# import pymystem3
+#
+# copypasta = ''
+# punct = '''!"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~'''
+# text = ''
+# for char in copypasta.lower():
+#     if char not in punct:
+#         text += char
+#
+# print(text)
+#
+# analysed = pymystem3.Mystem().analyze(text)
+# counter = 0
+#
+# for i in analysed:
+#     if 'analysis' in i and i['analysis'] and 'gr' in i['analysis'][0] and i['analysis'][0]['gr'][:2] == 'S,':
+#         counter += 1
+#         print(i['analysis'][0])
+# print(counter)
