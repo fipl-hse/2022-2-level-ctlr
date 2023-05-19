cd749cd19fd65fbadcc62f3ecb06707f357676f1
Maydaymywaytoheaven <tobakunina@edu.hse.ru>
2023-05-19 09:34:15 +0500

    how are those files my biggest hang-ups

--- seminars/seminar1.py
+++ seminars/seminar1.py
@@ -13,7 +13,8 @@
 # counter = 0
 #
 # for i in analysed:
-#     if 'analysis' in i and i['analysis'] and 'gr' in i['analysis'][0] and i['analysis'][0]['gr'][:2] == 'S,':
+#     if 'analysis' in i and i['analysis'] and 'gr' in i['analysis'][0]
+#     and i['analysis'][0]['gr'][:2] == 'S,':
 #         counter += 1
 #         print(i['analysis'][0])
 # print(counter)
--- seminars/seminar2.py
+++ seminars/seminar2.py
@@ -4,8 +4,9 @@
 #
 # def main():
 #     response = requests.get('https://xn--80ady2a0c.xn--p1ai/calendar/2023/01/',
-#                             headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-#                                                    '(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'})
+#                             headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+#                             AppleWebKit/537.36 '
+#                             '(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'})
 #
 #     with open('article_tanya.html', 'w+', encoding='utf-8') as file:
 #         file.write(response.text)
